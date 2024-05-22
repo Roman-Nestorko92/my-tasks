da49bdee61b3c4fcde0fd61389e5c472b7c9100d
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2024-05-22 20:18:43 +0200

    the middle

---
+++ js/tasks.js
@@ -0,0 +1,147 @@
+const addItemsForm = document.getElementById("itemsForm");
+const itemsList = document.getElementById("taskList");
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function addItem(e) {
+  e.preventDefault();
+  const taskText = e.target.item.value;
+  const item = {
+    text: taskText,
+    checked: false,
+  };
+  items.push(item);
+  localStorage.setItem("items", JSON.stringify(items));
+  displayItems(items, itemsList);
+  this.reset();
+}
+
+function removeFromList(idx) {
+  items.splice(idx, 1);
+  localStorage.setItem("items", JSON.stringify(items));
+  displayItems(items, itemsList);
+}
+
+function displayItems(tasks, taskList) {
+  taskList.innerHTML = tasks
+    .map((task, index) => {
+      return `<li class="taskItemBox">
+      <div class="contentBoxItem">
+      <span class="task-number">${index + 1}.</span>
+      <label for="item${index}">${task.text}</label>
+      </div>
+      <div class="taskButtonsBox">      
+      <input class="inputChecked" type="checkbox" id="item${index}" data-index="${index}" ${
+        task.checked ? "checked" : ""
+      } />
+      <label class="custom-checkbox" for="item${index}"></label>
+      <button class="removeButton" type="button" onClick="removeFromList(${index})">-</button>
+      </div>
+      </li>`;
+    })
+    .join("");
+}
+
+function toggleClick(e) {
+  if (!e.target.matches("input")) return;
+  const element = e.target.dataset.index;
+  items[element].checked = !items[element].checked;
+  localStorage.setItem("items", JSON.stringify(items));
+  displayItems(items, itemsList);
+}
+
+addItemsForm.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleClick);
+displayItems(items, itemsList);
+
+// // збереження в локал
+// function saveTask(task) {
+//   localStorage.setItem("task", JSON.stringify(task));
+// }
+
+// // завантаження з локал
+// function loadTask() {
+//   const task = JSON.parse(localStorage.getItem("task")) || [];
+//   return task;
+// }
+
+// // очищення списку
+// function emptyList(tasks) {
+//   taskList.innerHTML = "";
+
+//   tasks.forEach((el) => {
+//     const newTask = createTaskElement(el);
+//     taskList.appendChild(newTask);
+//   });
+// }
+
+// // створення замітки
+// function createTaskElement(taskContent) {
+//   const newTask = document.createElement("li");
+
+//   const tasksList = document.createElement("ul");
+//   tasksList.classList.add("task-item-list");
+
+//   const taskListItem = document.createElement("li");
+//   taskListItem.textContent = taskContent;
+//   tasksList.appendChild(taskListItem);
+
+//   const buttonsBox = document.createElement("li");
+//   buttonsBox.classList.add("buttonBox");
+
+//   const removeButton = document.createElement("button");
+//   removeButton.classList.add("buttonRemote");
+//   removeButton.textContent = "-";
+//   removeButton.addEventListener("click", () => {
+//     removeFromList(taskContent);
+//   });
+
+//   const doneButton = document.createElement("button");
+//   doneButton.classList.add("buttonDone");
+//   doneButton.textContent = "done";
+//   doneButton.addEventListener("click", () => {
+//     doneButton.classList.toggle("done");
+//     localStorage.setItem("doneState", JSON.stringify(doneButton));
+//   });
+
+//   buttonsBox.appendChild(removeButton);
+//   buttonsBox.appendChild(doneButton);
+
+//   tasksList.appendChild(buttonsBox);
+
+//   newTask.appendChild(tasksList);
+
+//   return newTask;
+// }
+
+// // додавання замітки
+
+// function addTaskToList(cont) {
+//   const task = loadTask();
+//   task.push(cont);
+//   saveTask(task);
+//   emptyList(task);
+// }
+
+// // видалення
+// function removeFromList(cont) {
+//   const task = loadTask().filter((task) => task !== cont);
+//   saveTask(task);
+//   emptyList(task);
+// }
+
+// taskForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const content = taskInput.value.trim();
+
+//   if (content !== "") {
+//     addTaskToList(content);
+//     taskInput.value = "";
+//   }
+// });
+
+// // завантаження
+// document.addEventListener("DOMContentLoaded", () => {
+//   const tasks = loadTask();
+//   emptyList(tasks);
+// });
